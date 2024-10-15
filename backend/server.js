3ab02ca5b5f961a349177cae00836487bad2b1bd
yaxnn <yaseenharish@gmail.com>
2024-10-16 00:37:21 +0530

    1

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+const app = express();
+
+app.get("/products", (req, res) => {});
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("server started at http://localhost:5000");
+});
