ad4855886275519731725e6a13aa511023598afa
yaxnn <yaseenharish@gmail.com>
2024-10-22 22:30:25 +0530

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Box,useColorModeValue } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
 import CreatePage from "./pages/CreatePage";
 import HomePage from "./pages/HomePage";
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <Box minH={"100vh"}>
+      <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
